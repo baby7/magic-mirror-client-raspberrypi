a5dda934ad2a5b896b3852ef6e49bf500425e7e0
xiang <1160084407@qq.com>
2020-05-02 16:21:08 +0800

    增加线上情绪识别

--- core/face_detection/face_detection.py
+++ core/face_detection/face_detection.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import os
 import cv2
 import time
 from conf import settings
+import requests
 
 path = settings.OPEN_CV_DIR    # open-cv文件夹位置
 focus = settings.FOCUS_FACE     # 人脸识别文件名称
-print(path + focus + ".xml")
 # 创建 classifier
 clf = cv2.CascadeClassifier(path + focus + ".xml")
 
@@ -26,14 +27,26 @@ def recognition(image):
     )
     # 判断是否有人脸
     if not len(faces) is 0:
-        something()
+        face_path = str(int(round(time.time() * 1000))) + ".png"
+        cv2.imwrite(face_path, image)
+        emotion_recognition(face_path)
     else:
         print("Null")
 
 
-# something
-def something():
-    print("检测到人脸")
+# 进行情绪识别
+def emotion_recognition(face_path):
+    url = "http://3cw0735693.qicp.vip/emotionRecognition"
+    file = open(face_path, 'rb')
+    # 拼接参数
+    files = {'file': (str(int(round(time.time() * 1000))), file, 'image/jpg')}
+    # 发送请求
+    print("send face data success")
+    res = requests.post(url, files=files)
+    # 获取服务器返回的信息
+    print(res.json())
+    file.close()
+    os.remove(face_path)
 
 
 def face_detection():
@@ -43,9 +56,11 @@ def face_detection():
         if ret is True:
             # 输出当前帧
             recognition(frame)
-            time.sleep(1)
         else:
             break
+        time.sleep(3)
     # 释放资源
     cap.release()
     cv2.destroyAllWindows()
+
+face_detection()
