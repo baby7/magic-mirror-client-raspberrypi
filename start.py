d9bcabe6faa7d45048ff9939405588cce4cee38b
xiang <1160084407@qq.com>
2020-06-17 17:03:40 +0800

    启动方式修改

--- start.py
+++
@@ -1,9 +0,0 @@
-import time
-from subprocess import Popen
-import os
-
-
-Popen(args='/home/pi/.local/bin/hass', shell=True)
-time.sleep(60)
-os.system("cd client-raspberrypi/bin/")
-os.system("python3 start.py")
---
+++ start.sh
@@ -0,0 +1,4 @@
+/home/pi/.local/bin/hass -parameters &
+sleep 30
+cd client-raspberrypi/bin/
+python3 start.py
