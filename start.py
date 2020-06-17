85655a7e626a55852268c11b009efd4a8bd52baa
xiang <1160084407@qq.com>
2020-06-17 16:41:25 +0800

    启动方式修改

--- start.py
+++ start.py
@@ -5,4 +5,5 @@ import os
 
 Popen(args='/home/pi/.local/bin/hass', shell=True)
 time.sleep(60)
-os.system("python3 client-raspberrypi/bin/start.py")
+os.system("cd client-raspberrypi/bin/")
+os.system("python3 start.py")
