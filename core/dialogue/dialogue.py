5ea8bb3b25c3e7adbc843dafc2cdfda11025663f
xiang <1160084407@qq.com>
2020-06-22 21:11:47 +0800

    系统升级

--- core/dialogue/dialogue.py
+++ core/dialogue/dialogue.py
@@ -112,8 +112,6 @@ def commend(text=""):
     # ************* system start *************
     if text == '系统升级':
         hassapi.system_upgrade()
-        play()
-        speak("开始系统升级")
     # ************* system end   *************
     return False
 
