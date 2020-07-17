785198c18eb0f3fcf55f6752d48aacb466b8fc42
xiang <1160084407@qq.com>
2020-07-17 15:04:24 +0800

    启动mqtt

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 /home/pi/.local/bin/hass &
+cd MiTemperature2/
+nohup python3 LYWSD03MMC.py --callback sendtoMQTT.sh -b 1 --name mimeter -d A4:C1:38:84:A5:63 &
 sleep 30
 cd /home/pi/client-raspberrypi/bin/
 python3 start.py
